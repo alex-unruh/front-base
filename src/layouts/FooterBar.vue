a084175b4d67af5644b8899cbaed1653625c80d2
Alexandre Unruh <alexandre@unruh.com.br>
2023-11-16 16:50:34 -0300

    Change top bar buttons and Footer text

--- src/layouts/FooterBar.vue
+++ src/layouts/FooterBar.vue
@@ -1,9 +1,9 @@
 <template>
-  <q-footer elevated :class="{ 'bg-dark': app.dark.isActive }">
-    <q-toolbar class="justify-center text-uppercase text-weight-bold" :class="{ 'text-teal': app.dark.isActive }">
-      Unruh Simple Test v0.1.0
-    </q-toolbar>
-  </q-footer>
+	<q-footer elevated :class="{ 'bg-dark': app.dark.isActive }">
+		<q-toolbar class="justify-center text-uppercase text-weight-bold" :class="{ 'text-teal': app.dark.isActive }">
+			Unruh Front Base v0.1.0
+		</q-toolbar>
+	</q-footer>
 </template>
 
 <script setup>
--- src/layouts/TopBar.vue
+++ src/layouts/TopBar.vue
@@ -1,24 +1,24 @@
 <template>
-  <q-header elevated>
-    <q-toolbar :class="{ 'bg-dark': app.dark.isActive }">
-      <q-btn flat dense round icon="menu" aria-label="Menu" @click="store.toggleLeftDrawer"
-        :class="{ 'text-teal': app.dark.isActive }" />
+	<q-header elevated>
+		<q-toolbar :class="{ 'bg-dark': app.dark.isActive }">
+			<q-btn flat dense round icon="menu" aria-label="Menu" @click="store.toggleLeftDrawer"
+				:class="{ 'text-teal': app.dark.isActive }" />
 
-      <q-toolbar-title class="q-pl-md">
-        <q-btn round dense icon="home" title="To Do" :to="{ name: 'index' }"
-          :class="{ 'text-teal': app.dark.isActive }" />
-      </q-toolbar-title>
+			<q-toolbar-title class="q-pl-md">
+				<q-btn round flat dense icon="home" title="To Do" :to="{ name: 'index' }"
+					:class="{ 'text-teal': app.dark.isActive }" />
+			</q-toolbar-title>
 
-      <q-btn round flat icon="contrast" class="q-mr-xs" @click="toggleDark()" title="Dark/Light"
-        :style="{ color: backgroundColor }" />
-      <q-btn round flat icon="account_circle" title="My Profile" :style="{ color: backgroundColor }" />
-      <q-btn round flat icon="power_settings_new" title="Logout" :style="{ color: backgroundColor }" />
-    </q-toolbar>
-  </q-header>
+			<q-btn round flat dense icon="contrast" class="q-mr-xs" @click="toggleDark()" title="Dark/Light"
+				:style="{ color: backgroundColor }" />
+			<q-btn round flat dense icon="account_circle" title="My Profile" :style="{ color: backgroundColor }" />
+			<q-btn round flat dense icon="power_settings_new" title="Logout" :style="{ color: backgroundColor }" />
+		</q-toolbar>
+	</q-header>
 
-  <q-dialog v-model="confirm" persistent>
-    <CardConfirm />
-  </q-dialog>
+	<q-dialog v-model="confirm" persistent>
+		<CardConfirm />
+	</q-dialog>
 </template>
 
 <script setup>
@@ -33,7 +33,7 @@ const confirm = ref(false);
 const backgroundColor = computed(() => app.dark.isActive ? 'teal' : '')
 
 function toggleDark() {
-  app.dark.toggle();
+	app.dark.toggle();
 }
 </script>
 
